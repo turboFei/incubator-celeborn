d6b1a7c5e9cfe8a5f4f3500ae91984144aad6fd2
Fei Wang <fwang12@ebay.com>
2024-07-03 09:24:19 -0700

    fix

--- service/src/main/scala/org/apache/celeborn/server/common/http/api/ApiBaseResource.scala
+++ service/src/main/scala/org/apache/celeborn/server/common/http/api/ApiBaseResource.scala
@@ -19,6 +19,7 @@ package org.apache.celeborn.server.common.http.api
 
 import javax.ws.rs.{GET, Path, Produces, QueryParam}
 import javax.ws.rs.core.MediaType
+
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
