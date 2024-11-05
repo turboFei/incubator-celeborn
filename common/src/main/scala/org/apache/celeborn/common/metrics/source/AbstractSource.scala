c3c327ef48426db696be486c9666f956bbc1329e
Wang, Fei <fwang12@ebay.com>
2024-11-05 15:55:41 -0800

    refine

--- common/src/main/scala/org/apache/celeborn/common/metrics/source/AbstractSource.scala
+++ common/src/main/scala/org/apache/celeborn/common/metrics/source/AbstractSource.scala
@@ -121,7 +121,7 @@ abstract class AbstractSource(conf: CelebornConf, role: String)
   def markMeter(name: String, value: Long, labels: Map[String, String] = Map.empty): Unit = {
     namedMeters.computeIfAbsent(
       metricNameWithCustomizedLabels(name, labels),
-      _ => NamedMeter(name, metricRegistry.meter(name), labels ++ staticLabels))
+      n => NamedMeter(name, metricRegistry.meter(n), labels ++ staticLabels))
       .meter.mark(value)
   }
 
