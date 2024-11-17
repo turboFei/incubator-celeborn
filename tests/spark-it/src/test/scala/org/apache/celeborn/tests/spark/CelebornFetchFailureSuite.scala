a230cbd0bd330671babf3a681fc002ac546d23c3
Wang, Fei <fwang12@ebay.com>
2024-11-17 14:43:33 -0800

    save

--- tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/CelebornFetchFailureSuite.scala
+++ tests/spark-it/src/test/scala/org/apache/celeborn/tests/spark/CelebornFetchFailureSuite.scala
@@ -23,6 +23,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 import org.apache.spark.{BarrierTaskContext, ShuffleDependency, SparkConf, SparkContextHelper, SparkException, TaskContext}
 import org.apache.spark.celeborn.ExceptionMakerHelper
 import org.apache.spark.rdd.RDD
+import org.apache.spark.scheduler.SparkSchedulerHelper
 import org.apache.spark.shuffle.ShuffleHandle
 import org.apache.spark.shuffle.celeborn.{CelebornShuffleHandle, ShuffleManagerHook, SparkShuffleManager, SparkUtils, TestCelebornShuffleManager}
 import org.apache.spark.sql.SparkSession
@@ -56,7 +57,8 @@ class CelebornFetchFailureSuite extends AnyFunSuite
     super.createWorker(map, storageDir)
   }
 
-  class ShuffleReaderGetHook(conf: CelebornConf) extends ShuffleManagerHook {
+  class ShuffleReaderGetHook(conf: CelebornConf, speculation: Boolean = false)
+    extends ShuffleManagerHook {
     var executed: AtomicBoolean = new AtomicBoolean(false)
     val lock = new Object
 
@@ -65,6 +67,10 @@ class CelebornFetchFailureSuite extends AnyFunSuite
         startPartition: Int,
         endPartition: Int,
         context: TaskContext): Unit = {
+      val taskIndex = SparkSchedulerHelper.getTaskIndex(context.taskAttemptId())
+      if (speculation && taskIndex == 0) {
+        Thread.sleep(3000) // sleep for speculation
+      }
       if (executed.get() == true) return
 
       lock.synchronized {
@@ -82,17 +88,19 @@ class CelebornFetchFailureSuite extends AnyFunSuite
             val allFiles = workerDirs.map(dir => {
               new File(s"$dir/celeborn-worker/shuffle_data/$appUniqueId/$celebornShuffleId")
             })
-            val datafile = allFiles.filter(_.exists())
-              .flatMap(_.listFiles().iterator).headOption
-            datafile match {
-              case Some(file) => file.delete()
-              case None => throw new RuntimeException("unexpected, there must be some data file" +
-                  s" under ${workerDirs.mkString(",")}")
+            val datafiles = allFiles.filter(_.exists())
+            if (datafiles.nonEmpty) {
+              if (taskIndex == 0) { // only cleanup the data file in the task with index 0
+                datafiles.foreach(_.delete())
+                executed.set(true)
+              }
+            } else {
+              throw new RuntimeException("unexpected, there must be some data file" +
+                s" under ${workerDirs.mkString(",")}")
             }
           }
           case _ => throw new RuntimeException("unexpected, only support RssShuffleHandle here")
         }
-        executed.set(true)
       }
     }
   }
@@ -457,7 +465,7 @@ class CelebornFetchFailureSuite extends AnyFunSuite
     val shuffleMgr = SparkContextHelper.env
       .shuffleManager
       .asInstanceOf[TestCelebornShuffleManager]
-    var preventShuffleFetchFailure = false
+    var preventUnnecessaryStageRerun = false
     val lifecycleManager = shuffleMgr.getLifecycleManager
     lifecycleManager.registerReportTaskShuffleFetchFailurePreCheck(new java.util.function.Function[
       java.lang.Long,
@@ -465,23 +473,23 @@ class CelebornFetchFailureSuite extends AnyFunSuite
       override def apply(taskId: java.lang.Long): Boolean = {
         val anotherRunningOrSuccessful = SparkUtils.taskAnotherAttemptRunningOrSuccessful(taskId)
         if (anotherRunningOrSuccessful) {
-          preventShuffleFetchFailure = true
+          preventUnnecessaryStageRerun = true
         }
         !anotherRunningOrSuccessful
       }
     })
 
     val celebornConf = SparkUtils.fromSparkConf(sparkSession.sparkContext.getConf)
-    val hook = new ShuffleReaderGetHook(celebornConf)
+    val hook = new ShuffleReaderGetHook(celebornConf, speculation = true)
     TestCelebornShuffleManager.registerReaderGetHook(hook)
 
     val value = Range(1, 10000).mkString(",")
     val tuples = sparkSession.sparkContext.parallelize(1 to 10000, 2)
-      .map { i => (i, value) }.groupByKey(16).collect()
+      .map { i => (i, value) }.groupByKey(2).collect()
 
     // verify result
     assert(hook.executed.get() == true)
-    assert(preventShuffleFetchFailure)
+    assert(preventUnnecessaryStageRerun)
     assert(tuples.length == 10000)
     for (elem <- tuples) {
       assert(elem._2.mkString(",").equals(value))
